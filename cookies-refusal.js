0b7d6b47f95b6a82ac1b1681afe2a19a2010ba0b
chalios <44355045+chalios@users.noreply.github.com>
2024-04-22 22:14:07 +0000

    Initial commit

---
+++ cookies-refusal.js
@@ -0,0 +1,57 @@
+// Cookies refusal
+// Initially made by chalios°
+
+/* 
+    This code is a workaround to uncheck every checkboxes in cookie consent panels.
+
+    Industrials tries to force users to consent to cookies by using a well known
+    social engineering technique : fatigue, human laziness.
+    So to circumvent this evident attack to reglementation and user rights, this code allows
+    to uncheck all checkboxes in cookie management panel of any site.
+
+    The procedure is :
+
+    - At presentation of cookie management panel, often after clicking "set preferences", 
+    open the developper panel of your browser : (fn)F12 on Chrome or Firefox,⌥⌘C sur Safari sur Mac...
+
+    - Make sure to have the panel with checkboxes visible onscreen.
+
+    - copy/paste the code to the javascript console of your browser and press Enter.
+
+    - Click on the button on cookie panel to save your preferences. 
+    (Should I mention that it is wise to visually check that each box has been unchecked before confirming?)
+
+    And you're done! You can close developper panel.
+*/
+
+/* 
+    This code is public domain.
+    Contribution is welcome. As redistribution, reappropriation, refactorization...
+
+    For example you could add more classnames to the classnames array below.
+    It's the class of the <input checkbox ...> that are in setting panel. 
+    Use the inspect command in browser's developper tools (right-click -> inspect) to find it
+    easily with your mouse.
+*/
+
+// Identified checkboxes classnames in google-styled cookie consent panel
+let classnames = [
+    'fc-preference-legitimate-interest', 
+    'fc-preference-consent'
+]
+
+// Loop over classnames to get every checkbox
+for (const classname of classnames) {
+    let checkboxes = document.getElementsByClassName(classname);
+
+    // If checkboxes with classname are found, loop over.
+    // If not, it loops over nothing. So it stops.
+    for (const checkbox of checkboxes) {
+
+        // Ensure you uncheck the box for the program, not just visually.
+        checkbox.value = false;
+
+        // Now uncheck it visually.
+        checkbox.removeAttribute('checked');
+    }
+}
